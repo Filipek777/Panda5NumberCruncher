ee1d219327d004bfdade58c6f9d477843a08bd96
Filipek777 <104428857+Filipek777@users.noreply.github.com>
2022-11-21 16:19:07 +0000

    Update Panda5.py
    df[' '].value_counts() = counts the column

--- Panda5.py
+++ Panda5.py
@@ -2,4 +2,17 @@ import pandas as pd
 
 df = pd.read_csv('OctoberCSV.csv')
 
-print(df) 
+#df['Column'].value_counts()
+#Counts how many times x in column
+
+print("Terminal Count:")
+df['Terminal'].value_counts()
+
+print("Message Type Count:")
+df['Message Type'].value_counts()
+
+print("Reason Count:")
+df['Reason'].value_counts()
+
+print("Root cause Count:")
+df['Root cause Type'].value_counts()
