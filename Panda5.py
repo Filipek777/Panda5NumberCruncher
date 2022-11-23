d0efda2764254dbf5574cd67601116f9cc26b515
Filipek777 <104428857+Filipek777@users.noreply.github.com>
2022-11-23 10:34:04 +0000

    Update Panda5.py

--- Panda5.py
+++ Panda5.py
@@ -6,16 +6,16 @@ df = pd.read_csv('OctoberCSV.csv')
 #Counts how many times x in column
 
 print("Terminal Count:")
-df['Terminal'].value_counts(ascending=True)
+df['SITE'].value_counts(ascending=True)
 
 print("Message Type Count:")
-df['Message Type'].value_counts(ascending=True)
+df['MESSAGE'].value_counts(ascending=True)
 
 print("Reason Count:")
-df['Reason'].value_counts(ascending=True)
+df['REASON'].value_counts(ascending=True)
 
 print("Root cause Count:")
-df['Root cause Type'].value_counts(ascending=True)
+df['CAUSE'].value_counts(ascending=True)
 
 
 ## ^ but with site occurances
